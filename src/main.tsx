af0f75292c96b920581ffd728a84528abb429b7d
BaboonKing <changfan.xu.up@qq.com>
2022-10-20 23:18:24 +0800

    feat: use nested-routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { App } from "./App";
 import { ErrorPage } from "./components/ErrorPage";
 import "./index.css";
@@ -8,16 +8,28 @@ import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <div>
+        <App />
+        <h2>children page</h2>
+        <Outlet />
+      </div>
+    ),
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/page",
-    element: <div>page</div>,
-  },
-  {
-    path: "/page1",
-    element: <div>page1</div>,
+    children: [
+      {
+        index: true,
+        element: <div>enter "page" / "page1" to address line </div>,
+      },
+      {
+        path: "page",
+        element: <div>page</div>,
+      },
+      {
+        path: "page1",
+        element: <div>page1</div>,
+      },
+    ],
   },
 ]);
 
