3ea9a19994b3610583d7efccf50c6d995eba3cac
BaboonKing <changfan.xu.up@qq.com>
2022-10-24 21:08:53 +0800

    feat: add transition at route changes

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,9 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { animated, useTransition } from "@react-spring/web";
+import { ReactNode } from "react";
+
+import { useLocation, useOutlet } from "react-router-dom";
+
+const outletMap: Record<string, ReactNode> = {};
 
 export function WelcomeLayout() {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  const location = useLocation();
+
+  const transitions = useTransition(location.pathname, {
+    from: { transform: "translateX(100%)" },
+    enter: { transform: "translateX(0%)" },
+    leave: { transform: "translateX(-100%)" },
+    config: {
+      duration: 500,
+    },
+  });
+
+  const outlet = useOutlet();
+
+  outletMap[location.pathname] = outlet;
+
+  return transitions((style, pathname) => (
+    <animated.div style={style} key={pathname}>
+      <div
+        style={{ textAlign: "center", border: "1px solid", padding: "10px" }}
+      >
+        {outletMap[pathname]}
+      </div>
+    </animated.div>
+  ));
 }
