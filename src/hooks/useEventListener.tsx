5f01f879b446a82465c730a0e3d4133555d9163e
BaboonKing <changfan.xu.up@qq.com>
2022-11-11 22:11:38 +0800

    refactor: rewrite useEventListener

--- src/hooks/useEventListener.tsx
+++ src/hooks/useEventListener.tsx
@@ -1,3 +1,5 @@
+import { RefObject, useEffect } from "react";
+
 export interface UseEventListenerOptions extends AddEventListenerOptions {}
 
 type EventMap<T> = T extends HTMLElement
@@ -9,22 +11,36 @@ type EventMap<T> = T extends HTMLElement
   : never;
 
 export const useEventListener = <
-  Target extends HTMLElement | Document | Window,
-  EventType extends keyof EventMap<Target>
+  Target extends RefObject<HTMLElement | Document | Window | null>,
+  EventType extends keyof EventMap<Target["current"]>
 >(
   target: Target,
   eventType: EventType,
-  listener: (this: Target, event: EventMap<Target>[EventType]) => any,
+  listener: (
+    this: Target,
+    event: EventMap<Target["current"]>[EventType]
+  ) => any,
   options?: boolean | UseEventListenerOptions
 ) => {
-  if (!target) {
-    return;
-  }
-
-  target.addEventListener(eventType as any, listener as any, options);
-
   const cleanup = () => {
-    target.removeEventListener(eventType as any, listener as any, options);
+    if (!target.current) {
+      return;
+    }
+    target.current.removeEventListener(
+      eventType as any,
+      listener as any,
+      options
+    );
   };
+
+  useEffect(() => {
+    if (!target.current) {
+      return;
+    }
+    target.current.addEventListener(eventType as any, listener as any, options);
+
+    return cleanup;
+  });
+
   return cleanup;
 };
